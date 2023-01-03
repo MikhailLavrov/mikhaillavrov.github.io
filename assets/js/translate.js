3d2b4e1f1b34104d39ad520d171bf5ea68cc05d4
Mikhail Lavrov <lavrov.fd@gmail.com>
2023-01-03 19:04:07 +0400

    add en translation

---
+++ assets/js/translate.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const translation = document.querySelector('#translation');
+  const ruToggle = translation.querySelector('#ruTranslate');
+  const enToggle = translation.querySelector('#enTranslate');
+
+  translation.addEventListener('click', (evt) => {
+    // evt.preventDefault();
+
+    evt.target === enToggle ? toggleToEn() : null;
+    evt.target === ruToggle ? toggleToRu() : null;
+  });
+
+  const toggleToEn = () => {
+    enToggle.classList.add('active');
+    ruToggle.classList.remove('active');
+  };
+
+  const toggleToRu = () => {
+    ruToggle.classList.add('active');
+    enToggle.classList.remove('active');
+  };
+});
